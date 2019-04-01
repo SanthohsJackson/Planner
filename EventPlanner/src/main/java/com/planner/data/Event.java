b280ece4e9dbd6f7bf8252704f029e6a6f4e9308
Santhosh Jackson <santhosh.jack.j@gmail.com>
2019-04-01 14:18:42 +0100

    Added MongoDB

--- EventPlanner/src/main/java/com/planner/data/Event.java
+++ EventPlanner/src/main/java/com/planner/data/Event.java
@@ -1,19 +1,23 @@
 package com.planner.data;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
 public class Event {
 
-	private long id;
+	@Id
+	private ObjectId _id;
 
 	private String name;
 
-	private String time;
+	private String time;	
 
-	public long getId() {
-		return id;
+	public ObjectId get_id() {
+		return _id;
 	}
 
-	public void setId(long id) {
-		this.id = id;
+	public void set_id(ObjectId _id) {
+		this._id = _id;
 	}
 
 	public String getName() {
